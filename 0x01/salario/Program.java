3ff34a54a5ba73f6d6a01d5f620d1a66fb65f501
devdelucca <deluccaby@gmail.com>
2022-05-03 22:40:31 -0300

    projeto CalculadorSalario

--- 0x01/salario/Program.java
+++ 0x01/salario/Program.java
@@ -1,5 +1,5 @@
 public class Program {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         SalarioService salarioService1 = new SalarioService();
         SalarioService salarioService2 = new SalarioService();
 
@@ -7,7 +7,7 @@ public class Program {
         double valorSalario2 = salarioService2.calcular(2500, 350, 10000, 15);
 
         System.out.printf("Salario 1: %.2f\n", valorSalario1);
-        System.out.print("UUID 1: %s\n", salarioService1.getUuid());
+        System.out.printf("UUID 1: %s\n\n", salarioService1.getUuid());
 
         System.out.printf("Salario 2: %.2f\n", valorSalario2);
         System.out.printf("UUID 2: %s\n", salarioService2.getUuid());
